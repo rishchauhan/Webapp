37e83dc645a4485f9becf15365b44178f585fdf3
Abinaya <Abinaya@DESKTOP-ESVS1FM>
2023-10-03 00:49:31 +0530

    Add project files.

---
+++ Webapp/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Webapp.Services;
+using Webapp.Model;
+
+namespace Webapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+
+        }
+    }
+}
---
+++ Webapp/Services/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using Webapp.Model;
+
+namespace Webapp.Services
+{
+
+    // This service will interact with our Product data in the SQL database
+    public class ProductService
+    {
+        private static string db_source = "sqlserverrish.database.windows.net";
+        private static string db_user = "sqladminrish";
+        private static string db_password = "Rish@2310chouhan";
+        private static string db_database = "sqldbRish";
+
+        private SqlConnection GetConnection()
+        {
+
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+        public List<Product> GetProducts()
+        {
+            List<Product> _product_lst = new List<Product>();
+            string _statement = "SELECT ProductID,ProductName,Quantity from Products";
+            SqlConnection _connection = GetConnection();
+            _connection.Open();
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Product _product = new Product()
+                    {
+                        ProductID = _reader.GetInt32(0),
+                        ProductName = _reader.GetString(1),
+                        Quantity = _reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(_product);
+                }
+            }
+            _connection.Close();
+            return _product_lst;
+        }
+
+    }
+}
+
